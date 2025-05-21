ca493606f955b20882c157a7acc93ea2658d9e94
Ibne Sabid saikat <92563381+21saikat@users.noreply.github.com>
2025-05-21 10:02:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from GitHub CI/CD on Azure VM!'
+    return 'Hello from GitHub CI/CD on Azure VM update !'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
